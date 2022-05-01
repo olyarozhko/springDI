c3f9ad4ea58c3b069ac13f4df8cb4acaea11ea6e
orozhko <olga.rpzhko@microfocus.com>
2022-05-01 21:17:36 +0300

    hw4. code formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ import services.PlusService;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Xml-configuration:");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
@@ -13,11 +15,11 @@ public class Main {
         PlusService plusService = context.getBean("plusServiceBean", PlusService.class);
         Calculator calculator = context.getBean("calculatorBean", Calculator.class);
 
-        System.out.println("Xml-configuration:");
         System.out.println("num1 = 3.0, num2 = 2.0: " + new Calculator(plusService, minusService)
                 .calculate(3.0, 2.0));
         System.out.println("num1 = 1.0, num2 = 2.0: " + calculator.calculate(1.0, 2.0));
 
+//-------------------------------------------------------------------------
         System.out.print("\nJava Annotations-configuration:\n");
         ApplicationContext appContext = new AnnotationConfigApplicationContext("services");
         System.out.println("num1 = 2.5, num2 = 4.5: " + new Calculator(appContext.getBean(PlusService.class),
@@ -25,6 +27,7 @@ public class Main {
         System.out.println("num1 = 5.5, num2 = 3.5: " + new Calculator(appContext.getBean(PlusService.class),
                 appContext.getBean(MinusService.class)).calculate(5.5, 3.5));
 
+//-------------------------------------------------------------------------
         System.out.print("\nJava Code-configuration:\n");
         ApplicationContext appContext2 = new AnnotationConfigApplicationContext(Calculator2Config.class);
         System.out.println("num1 = 5, num2 = 4: " + appContext2.getBean(Calculator2.class).calculate(5, 4));
